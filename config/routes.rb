a4577a6ecb17c020aab49bdedd991f9834bf77cf
Tingting Wei <twei3@academic.rrc.ca>
2024-02-13 13:24:03 -0600

    Create a model called Product.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get "/products", to: "products#index"
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
+Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
