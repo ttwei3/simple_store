06682264ad177bea261c3d83f45a8f6a743cbf59
Tingting Wei <twei3@academic.rrc.ca>
2024-02-14 19:46:56 -0600

    Rewrite db/seeds.rb file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,7 @@
 #   end
 require "csv"
 
+=begin
 676.times do
   Product.create!(
     title: Faker::Commerce.product_name,
@@ -16,3 +17,32 @@ require "csv"
     stock_quantity: Faker::Number.number
   )
 end
+=end
+
+Product.destroy_all
+
+Category.destroy_all
+
+csv_file = Rails.root.join('db/products.csv')
+csv_data = File.read(csv_file)
+
+products = CSV.parse(csv_data, headers: true)
+
+# If CSV was created by Excel in Windows you may also need to set an encoding type:
+# products = CSV.parse(csv_data, headers: true, encoding: 'iso-8859-1')
+
+products.each do |product|
+  category_name = product[:category]
+  category_name = category_name.split(',').first if category_name&.include?(',')
+  category = Category.find_or_create_by(name: category_name) if category_name
+
+  Product.create(
+    title: product[:name],
+    description: product[:description],
+    price: product[:price],
+    stock_quantity: product[:stock_quantity],
+    category: category
+  )
+end
+
+# Where "category_name" is the category name as a string. You will need to get this from the data returned from the csv library.
